f2242fc998f138aad1bb059863d809f01eb0f87b
Jetterko <jetter.gameplayer@gmail.com>
2024-11-23 20:35:32 +0100

    nový script
    nový script, že když se klikne na 'PLAY', tak tě to přenese na jinou mapu, ale ještě předtím se musí něco udělat v Build Settings

---
+++ Assets/Scripty/novamapa.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using System.Collections;
+
+
+public class novamapa: MonoBehaviour
+{
+
+    void OnMouseDown()
+    {
+        Application.LoadLevel("SampleScene");
+    }
+
+}
