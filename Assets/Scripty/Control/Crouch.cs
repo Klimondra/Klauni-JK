33e003d9227be3027ba201c8638ea2e225278a1e
Jetterko <jetter.gameplayer@gmail.com>
2024-11-29 22:17:40 +0100

    přidáno krčení se
    když se stiskne a podrží klávesa C, hráč se skrčí

---
+++ Assets/Scripty/Control/Crouch.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crouch : MonoBehaviour
+{
+    public CharacterController PlayerHeight;
+    public float normalHeight, crouchHeight;
+
+    void Update () {
+        if (Input.GetKeyDown(KeyCode.C)){
+            PlayerHeight.height = crouchHeight;
+        }
+        if (Input.GetKeyUp(KeyCode.C)){
+            PlayerHeight.height = normalHeight;
+        }
+    }
+}
