93975c14858f68491f1e242a753800448a74a1ad
Klimondra <klimondra@seznam.cz>
2024-12-03 21:30:58 +0100

    Priprava na pridani cutsceny
    Took 19 minutes

---
+++ Assets/Scripty/Triggers/Prolog/PrologDoorCutscene.cs
@@ -0,0 +1,37 @@
+using TMPro;
+using UnityEngine;
+using TMPro;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+
+public class PrologDoorCutscene : MonoBehaviour
+{
+
+    public Transform playerTrans;
+    public Transform triggerPoint;
+    public TMP_Text textHint;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        textHint.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance(triggerPoint.position, playerTrans.position) < 1f)
+        {
+            textHint.enabled = true;
+
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                SceneManager.LoadScene("");
+            }
+        }
+        else
+        {
+            textHint.enabled = false;
+        }
+    }
+}
